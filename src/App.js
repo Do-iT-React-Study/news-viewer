feace24056877811409190c27921a8064a70e5be
SangJin521 <rhrhghrhrhgh@ajou.ac.kr>
2023-11-16 19:37:51 +0900

    [sangjin] 6회차 출석과제

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
+import axios from "../node_modules/axios/index";
+import { NewsItem } from "./NewsItem";
+import { NewsList } from "./NewsList";
 
 function App() {
+
+  
+
+  // const [data, setData] = useState(null);
+
+  // const onClickAPI = () => {
+  //   axios
+  //     .get("https://newsapi.org/v2/top-headlines?country=kr&apiKey=b2b645f6f4d343c592e893ca0ae71ff1")
+  //     .then((response) => {
+  //     setData(response.data);
+  //   });
+  // };
   return (
-    <>
-      <div>
-        <button>불러오기</button>
+    <div>
+      <NewsList/>
+      {/* <NewsItem article = {sampleData}/> */}
+      {/* <div>
+        <button onClick={onClickAPI}>불러오기</button>
       </div>
-    </>
+      {data && (
+        <textarea 
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}/>
+      )} */}
+      
+    </div>
   );
 }
 
---
+++ src/NewsList.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { NewsItem } from "./NewsItem";
+import axios from "../node_modules/axios/index";
+
+export const NewsList = () => {
+
+    const [article, setArticle] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get("https://newsapi.org/v2/top-headlines?country=kr&apiKey=b2b645f6f4d343c592e893ca0ae71ff1");
+
+                console.log(res.data);
+                setArticle(res.data.articles);
+            } catch (e) {
+                console.error("error:",e);
+            }
+        };
+        fetchData();
+    },[]);
+
+    const sampleData = {
+        title:"제목",
+        description:"내용",
+        url:"https://www.google.com",
+        urlToImage:"./unnamed.jpg"
+      };
+
+    return (
+        <>
+            {article &&
+                article.map((data, idx) => {
+                    return  (
+                    <NewsItem
+                        key={idx}
+                        article={data}/>)
+            })}
+            
+        </>
+    );
+};
