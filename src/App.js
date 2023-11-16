030b73e19edf2c571fc2c56e7c9ea6980a5affef
chamny20 <chamny20@naver.com>
2023-11-16 17:09:19 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -5,32 +5,12 @@ import axios from "../node_modules/axios/index";
 import { NewsList } from "./NewsList";
 
 function App() {
-  // const [data, setData] = useState(null);
-  // const onClick = async () => {
-  //   try {
-  //     const response = await axios.get(
-  //       "https://newsapi.org/v2/top-headlines?country=kr&apiKey=2d8dbec14f9149fea238b324c5540aa1",
-  //     );
-  //     setData(response.data);
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // };
-
   return (
-    <NewsList />
-    // <div>
-    //   <div>
-    //     <button onClick={onClick}>불러오기</button>
-    //   </div>
-    //   {data && (
-    //     <textarea
-    //       rows={7}
-    //       value={JSON.stringify(data, null, 2)}
-    //       readOnly={true}
-    //     />
-    //   )}
-    // </div>
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+    </div>
   );
 }
 
